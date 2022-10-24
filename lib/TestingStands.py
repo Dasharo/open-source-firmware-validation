3dd43ba7e60f16b370311dff8ce25955dfe8517d
Przemyslaw Sulewski <przemyslaw.sulewski@3mdeb.com>
2022-10-24 15:32:01 +0200

    lib/TestingStands.py: add variables for every msi testing stands
    Signed-off-by: Przemyslaw Sulewski <przemyslaw.sulewski@3mdeb.com>

--- lib/TestingStands.py
+++ lib/TestingStands.py
@@ -3,7 +3,21 @@ REMOTE TESTING ENVIRONMENT CONFIGS
 """
 rte_1 = {
     'device_type':          'RTE',
-    'rte_ip':               '192.168.4.39',
+    'device_ip':            '192.168.4.233',
+    'cpuid':                '02c0014266f49b55',
+    'pcb_rev':              '1.1.0'
+}
+
+rte_2 = {
+    'device_type':          'RTE',
+    'device_ip':            '192.168.4.248',
+    'cpuid':                '02c00042a74281e6',
+    'pcb_rev':              '1.1.0'
+}
+
+rte_3 = {
+    'device_type':          'RTE',
+    'device_ip':            '192.168.4.39',
     'cpuid':                '02c000424753a7fb',
     'pcb_rev':              '1.1.0'
 }
@@ -14,9 +28,39 @@ DEVICES UNDER TEST CONFIGS
 """
 device_1 = {
     'device_type':          'Device Under Test',
-    'platform_ip':          'not set',
+    'device_ip':            'not set',
     'platform':             'pro-z690-a-ddr5',
     'vendor':               'MSI Co., Ltd'
+}
+
+device_2 = {
+    'device_type':          'Device Under Test',
+    'device_ip':            'not set',
+    'platform':             'pro-z690-a-wifi-ddr4',
+    'vendor':               'MSI Co., Ltd'
+}
+
+device_3 = {
+    'device_type':          'Device Under Test',
+    'device_ip':            'not set',
+    'platform':             'pro-z690-a-wifi-ddr4',
+    'vendor':               'MSI Co., Ltd'
+}
+
+"""
+-------------------------------------------------------------------------------
+PiKVM DEVICES CONFIGS
+"""
+pikvm_1 = {
+    'device_type':          'PiKVM',
+    'device_name':          'PiKVM RPI0',
+    'device_ip':            '192.168.4.65',
+}
+
+pikvm_2 = {
+    'device_type':          'PiKVM',
+    'device_name':          'PiKVM RPI4',
+    'device_ip':            '192.168.4.222',
 }
 
 """
@@ -25,8 +69,20 @@ POWER CONTROL DEVICES CONFIGS
 """
 sonoff_1 = {
     'device_type':          'Sonoff',
-    'device_name':          'Sonoff S20 EU type E ',
-    'sonoff_ip':            '192.168.4.147'
+    'device_name':          'Sonoff S20 EU type E',
+    'device_ip':            '192.168.4.43'
+}
+
+sonoff_2 = {
+    'device_type':          'Sonoff',
+    'device_name':          'Sonoff S20 EU type E',
+    'device_ip':            '192.168.4.244'
+}
+
+sonoff_3 = {
+    'device_type':          'Sonoff',
+    'device_name':          'Sonoff S20 EU type E',
+    'device_ip':            '192.168.4.147'
 }
 
 """
@@ -83,9 +139,11 @@ tpm_1 = {
 -------------------------------------------------------------------------------
 TEST STANDS CONFIGURATIONS
 """
-config_1 = [rte_1, device_1, sonoff_1, usb_1, ssd_1]
+config_1 = [rte_1, device_1, sonoff_1, pikvm_1, usb_1, ssd_1]
+config_2 = [rte_2, device_2, sonoff_2, pikvm_2]
+config_3 = [rte_3, device_3, sonoff_3]
 
-configs = [config_1]
+configs = [config_1, config_2, config_3]
 
 """
 -------------------------------------------------------------------------------
@@ -99,14 +157,14 @@ class TestingStands(object):
         for config in configs:
             for device in config:
                 if device['device_type'] == 'RTE':
-                    if device['rte_ip'] == stand_ip:
+                    if device['device_ip'] == stand_ip:
                         return device['cpuid']
 
     def check_rte_provided_ip(self, stand_ip):
         for config in configs:
             for device in config:
                 if device['device_type'] == 'RTE':
-                    if device['rte_ip'] == stand_ip:
+                    if device['device_ip'] == stand_ip:
                         return True
         return False
 
@@ -114,20 +172,34 @@ class TestingStands(object):
         for config in configs:
             for device in config:
                 if device['device_type'] == 'Device Under Test':
-                    if device['platform_ip'] == stand_ip:
+                    if device['device_ip'] == stand_ip:
                         return True
         return False
 
     def get_power_control_method(self, stand_ip):
         for config in configs:
             if config[0]['device_type'] == 'RTE':
-                if config[0]['rte_ip'] == stand_ip:
+                if config[0]['device_ip'] == stand_ip:
                     for device in config:
                         if ['device_type'] == 'Sonoff':
                             return 'sonoff'
                     return 'rte'
             elif config[0]['device_type']== 'Device Under Test':
-                if config[0]['platform_ip'] == stand_ip:
+                if config[0]['device_ip'] == stand_ip:
                     for device in config:
                         if ['device_type'] == 'Sonoff':
                             return 'sonoff'
+
+    def get_pikvm_ip(self, stand_ip):
+        for config in configs:
+            if config[0]['device_type'] == 'RTE':
+                if config[0]['device_ip'] == stand_ip:
+                    for device in config:
+                        if device['device_type'] == 'PiKVM':
+                            return device['device_ip']
+                    return '0'
+            elif config[0]['device_type']== 'Device Under Test':
+                if config[0]['device_ip'] == stand_ip:
+                    for device in config:
+                        if device['device_type'] == 'PiKVM':
+                            return device['device_ip']
