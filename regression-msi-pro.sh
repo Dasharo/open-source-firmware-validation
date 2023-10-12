7e4ee47554697f0dfd79db5ee6b1b3636ed7d045
Piotr Lasota <piotr.lasota@3mdeb.com>
2023-10-12 13:57:28 +0200

    Psotas-regression-msi-pro-test
    Signed-off-by: Piotr Lasota <piotr.lasota@3mdeb.com>

--- regression-msi-pro.sh
+++ regression-msi-pro.sh
@@ -13,12 +13,12 @@ CONFIG="msi-pro-z690-a-wifi-ddr4"
 # CONFIG="msi-pro-z690-a-ddr5"
 # CONFIG="msi-pro-z790-p"
 
-FW_FILE="msi_ms7d25_ddr4_v1.1.2_serial.rom"
+# FW_FILE="msi_ms7d25_ddr4_v1.1.2_serial.rom"
 
-if [ ! -f "${FW_FILE}" ]; then
-    echo "${FW_FILE} not found. Please provide correct FW_FILE value"
-    exit 1
-fi
+# if [ ! -f "${FW_FILE}" ]; then
+#     echo "${FW_FILE} not found. Please provide correct FW_FILE value"
+#     exit 1
+# fi
 
 # Function to execute the robot command
 execute_robot() {
@@ -36,7 +36,6 @@ execute_robot() {
           -v rte_ip:${RTE_IP} \
           -v config:${CONFIG} \
           -v snipeit:no \
-          -v fw_file:${FW_FILE} \
           dasharo-${_category}/${_test_name}.robot
 }
 # Function to handle Ctrl+C
@@ -50,41 +49,41 @@ handle_ctrl_c() {
 trap 'handle_ctrl_c' SIGINT
 
 compatibility_tests=(
-  "efi"
-  "display-ports-and-lcd-support"
-  "usb-hid-and-msc-support"
-  "uefi-shell"
-  "dmidecode"
-  "logo-customization-functionality"
-  "custom-boot-menu-key"
-  "wifi-bluetooth-support"
-  "audio-subsystem"
-  "nvme-support"
-  "network-boot"
-  "uefi-shell"
-  "cpu-status"
+  # "efi"
+  # "display-ports-and-lcd-support"
+  # "usb-hid-and-msc-support"
+  # "uefi-shell"
+  # "dmidecode"
+  # "logo-customization-functionality"
+  # "custom-boot-menu-key"
+  # "wifi-bluetooth-support"
+  # "audio-subsystem"
+  # "nvme-support"
+  # "network-boot"
+  # "uefi-shell"
+  # "cpu-status"
   "reset-to-defaults"
-  "platform-suspend-and-resume"
-  "memory-profile"
+  # "platform-suspend-and-resume"
+  # "memory-profile"
 )
 
 security_tests=(
-  "tpm-support"
-  "measured-boot"
-  "verified-boot"
-  "network-stack"
-  "me-neuter"
-  "uefi-password"
-  "early-boot-dma-protection"
-  "tcg-opal-disk-password"
-  "usb-stack"
-  "bios-lock"
-  "smm-bios-write-protection"
+  # "tpm-support"
+  # "measured-boot"
+  # "verified-boot"
+  # "network-stack"
+  # "me-neuter"
+  # "uefi-password"
+  # "early-boot-dma-protection"
+  # "tcg-opal-disk-password"
+  # "usb-stack"
+  # "bios-lock"
+  # "smm-bios-write-protection"
 )
 
 performance_tests=(
-  "cpu-temperature"
-  "cpu-frequency"
+  # "cpu-temperature"
+  # "cpu-frequency"
 )
 
 ### Firmware + Ubuntu tests ###
