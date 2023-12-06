9e8aa879497688027748ae59da14428e62c5e04c
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-12-06 18:18:55 +0100

    regression-generic.sh: add generic regression script
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

---
+++ regression-generic.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# RTE_IP alone should be enough for running the regression.
+# If for some reason, Sonoff/PiKVM IPs are not correct, they
+# should be changed in the variables.robot file
+RTE_IP=${RTE_IP:=192.168.10.55}
+
+# Uncomment one of these
+CONFIG=${CONFIG:=protectli-v1210}
+
+FW_FILE=${FW_FILE:=protectli-v1210.rom}
+
+if [ ! -f "${FW_FILE}" ]; then
+    echo "${FW_FILE} not found. Please provide correct FW_FILE value"
+    exit 1
+fi
+
+# Function to execute the robot command
+execute_robot() {
+    local _category=$1
+    local _log_file="${_category}_log.html"
+    local _report_file="${_category}_report.html"
+    local _output_file="${_category}.xml"
+
+    robot -L TRACE \
+          -l ${CONFIG}/${_log_file} \
+          -r ${CONFIG}/${_report_file} \
+          -o ${CONFIG}/${_output_file} \
+          -v rte_ip:${RTE_IP} \
+          -v config:${CONFIG} \
+          -v fw_file:${FW_FILE} \
+          dasharo-${_category}
+}
+# Function to handle Ctrl+C
+handle_ctrl_c() {
+    echo "Ctrl+C pressed. Exiting."
+    # You can add cleanup tasks here if needed
+    exit 1
+}
+
+# Trap SIGINT (Ctrl+C)
+trap 'handle_ctrl_c' SIGINT
+
+execute_robot "compatibility"
+execute_robot "security"
+execute_robot "performance"
