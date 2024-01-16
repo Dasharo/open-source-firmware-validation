a8bbf1d14d6c682bcbe5d7e660d75548cae2e459
Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>
2024-01-16 17:38:09 +0100

    lib: secure-boot-lib: add Convert Mokutil To Openssl Output KW to compare certs
    Signed-off-by: Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>

---
+++ lib/secure-boot-lib.py
@@ -0,0 +1,27 @@
+from robot.api.deco import keyword
+
+
+@keyword("Convert Mokutil To Openssl Output")
+def convert_mokutil_to_openssl_output(file_content):
+    """
+    Converts mokutil output to openssl output. Returns the modified file
+    content.
+    """
+    lines = file_content.split("\n")
+    modified_content = ""
+    for line in lines:
+        if line.startswith("SHA1 Fingerprint"):
+            # Extract the fingerprint part, change it to uppercase and skip adding this line to modified content
+            continue
+            # fingerprint = line.replace("=", ":").upper()
+        elif line.startswith("[key 1]"):
+            continue
+        if line == lines[-1]:
+            modified_content += line
+        else:
+            # Add other lines to modified content and remove whitespaces around
+            # the '=' char
+            line = line.replace("=", " = ")
+            modified_content += line + "\n"
+    # Append the fingerprint to the end
+    return modified_content
