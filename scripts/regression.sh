a96d9f8d540783c050f9020bfb2e650b8acaaecc
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2024-03-15 14:03:44 +0100

    scripts/regression.sh: drop VP4650 vars
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-FW_FILE=dasharo-protectli-vp46xx-.rom
-DEVICE_IP=192.168.10.21
-RTE_IP=192.168.10.203
-CONFIG=protectli-vp4650
-
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIR}/lib/robot.sh"
 
