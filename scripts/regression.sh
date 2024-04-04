5b3db75f3ee0b51423dd87ba459542ccb6caeb64
Filip Lewiński <filip.lewinski@3mdeb.com>
2024-04-04 15:47:00 +0200

    platform-configs: add VP6650 support
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -3,14 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIR}/lib/robot.sh"
 
-FW_FILE=read-test.rom
-# DEVICE_IP=192.168.10.166
-RTE_IP=192.168.10.110
-CONFIG=protectli-vp6670
-SNIPEIT=no
-
 # FW_FILE and DEVICE_IP are required for full regression
 check_env_variable "FW_FILE"
 # check_env_variable "DEVICE_IP"
 
-execute_robot "dasharo-compatibility/flash-test-stub.robot"
+execute_robot "dasharo-compatibility"
