390279a11e6420ec6a9c356d26914176842c9c5e
Daniil Klimuk <daniil.klimuk@3mdeb.com>
2024-04-25 12:24:17 +0200

    scripts: qemu-run.sh: increase QEMU RAM to 8G
    Otherwise cloud-init will be killed by OOM-Killer during installation
    via netboot.xyz with autoinstall option.
    
    Signed-off-by: Daniil Klimuk <daniil.klimuk@3mdeb.com>

--- scripts/ci/qemu-run.sh
+++ scripts/ci/qemu-run.sh
@@ -179,9 +179,9 @@ case "${ACTION}" in
     check_disks ${ACTION}
     ;;
   os_install)
-    MEMORY="4G"
-    QEMU_PARAMS="${QEMU_PARAMS} ${QEMU_PARAMS_OS} ${QEMU_PARAMS_INSTALLER}"
-    check_disks ${ACTION}
+    MEMORY="8G"
+    QEMU_PARAMS="${QEMU_PARAMS_BASE} ${QEMU_PARAMS_OS} ${QEMU_PARAMS_INSTALLER}"
+    check_disks
     ;;
   *)
     echo "Action: ${ACTION} not supported"
