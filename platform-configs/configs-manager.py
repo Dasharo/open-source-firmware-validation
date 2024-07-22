427b59dd01de77f9ab65400edc199ddd94535935
Filip Gołaś <filip.golas@3mdeb.com>
2024-07-22 09:09:24 +0200

    platform-configs/../default.robot: Use TBD instead of EMPTY
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

---
+++ platform-configs/configs-manager.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+
+# Functionalities
+# [ ] Copy all variables from default.robot to new config
+# [ ] Update existing config with default variables from default
+# [ ]
+# [ ]
+# [ ]
+# [ ]
+# [ ]
+# [ ]
+# [ ]
+# [ ]
+
+
+
