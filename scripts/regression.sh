298c322d790b2de114f6853a51a00cffcbd170ac
Filip Gołaś <filip.golas@3mdeb.com>
2024-07-24 07:18:17 +0200

    scripts/regression.h: Add ability to pass additional args
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/lib/robot.sh"
 
 # FW_FILE and DEVICE_IP are required for full regression
@@ -9,6 +9,6 @@ check_env_variable "DEVICE_IP"
 
 check_test_station_variables
 
-execute_robot "dasharo-compatibility"
-execute_robot "dasharo-security"
-execute_robot "dasharo-performance"
+execute_robot "dasharo-compatibility" "${@}"
+execute_robot "dasharo-security" "${@}"
+execute_robot "dasharo-performance" "${@}"
