0d66d1d969d43bce832da4f3aa20c9ff6b6ce23e
WiktorG351 <141132416+WiktorG351@users.noreply.github.com>
2024-08-13 17:58:43 +0200

    Automatically generate Keywoards Documentation using libdoc with CI workflow (#352)
    * scripts/create-docs.sh: created
    
    * README.md: add info about libdoc, testdoc and create-docs.sh
    
    * .github/workflows/auto-docs.yml: added
    
    * adressed review
    
    * try to start workflow
    
    * fix paths
    
    * make sure virtual environment gets activated
    
    * modify libdoc command, remove venv activation
    
    * try to make it push html files
    
    * change the documentation file name
    
    * test whether the documentation gets updated
    
    * remove the unnecessary keyword since the deployment works
    
    * README.md: added info about github pages
    
    * README.md: add docs deployment status badge
    
    Signed-off-by: Artur Raglis <artur.raglis@3mdeb.com>
    
    * README.md: fix status badge link
    
    Signed-off-by: Artur Raglis <artur.raglis@3mdeb.com>
    
    ---------
    
    Signed-off-by: Artur Raglis <artur.raglis@3mdeb.com>
    Co-authored-by: Artur Raglis <artur.raglis@3mdeb.com>

---
+++ scripts/create-docs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+TEMP_DIR=$(mktemp -d)
+
+cat keywords.robot > "$TEMP_DIR/all-keywords.robot"
+find lib -name "*.robot" -exec cat {} + >> "$TEMP_DIR/all-keywords.robot"
+
+libdoc "$TEMP_DIR/all-keywords.robot" "$TEMP_DIR/all-keywords.html" >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "libdoc command failed"
+  exit 1
+fi
+
+cp "$TEMP_DIR/all-keywords.html" ./docs/index.html
+
+rm "$TEMP_DIR/all-keywords.robot"
+rm "$TEMP_DIR/all-keywords.html"
+rmdir "$TEMP_DIR"
+
+echo "Documentation generated and saved as ./docs/index.html"
