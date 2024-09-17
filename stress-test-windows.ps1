c046488a9f5b8179705ed26bdf33f46e38fa2986
Filip Gołaś <filip.golas@3mdeb.com>
2024-09-17 07:36:09 +0200

    stress-test-windows.ps1: Try making the test time bound
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- stress-test-windows.ps1
+++ stress-test-windows.ps1
@@ -1,11 +1,12 @@
-$numCores = 4
+$numCores = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
 
 foreach ($loopnumber in 1..$numCores){
+    $start_time = Get-Date
     Start-Job -ScriptBlock{
-    $result = 1
-        foreach ($number in 1..40000000){
-            $result = $result * $number
-        }
+    $result = 7
+        while (Get-Date) -lt ($start_time.AddMinutes(60)) { # Parametrize stress length
+            # Is this really the best operation to stress the system?
+            $result = $result * $result
     }
 }
 
