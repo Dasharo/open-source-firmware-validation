c80ee20659f74cb37da8b15ddd5c8e0cb3fe2c2d
Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
2024-12-03 15:17:30 +0100

    scripts/ci/ipxe-run.sh: Change to symlinks
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>

--- scripts/ci/ipxe-run.sh
+++ scripts/ci/ipxe-run.sh
@@ -6,18 +6,18 @@ if [[ $# -ne 2 ]]; then
 fi
 
 DTS_IMAGE_PATH=$1
-DTS_IMAGE_FILENAME=$( basename $DTS_IMAGE_PATH)
+DTS_IMAGE_FILENAME=$( basename $DTS_IMAGE_PATH )
 BZ_IMAGE_PATH=$2
+BZ_IMAGE_FILENAME=$( basename $BZ_IMAGE_PATH )
 IPXE_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/../../ipxe
 
-echo $IPXE_PATH
-mkdir $IPXE_PATH
-cp $DTS_IMAGE_PATH $IPXE_PATH
-cp $BZ_IMAGE_PATH $IPXE_PATH
+mkdir -p $IPXE_PATH
+ln -sf $DTS_IMAGE_PATH $IPXE_PATH/$DTS_IMAGE_FILENAME
+ln -sf $BZ_IMAGE_PATH $IPXE_PATH/$BZ_IMAGE_FILENAME
 
 echo -e "\n
 #!ipxe\n
-imgfetch --name file_kernel bzImage\n
+imgfetch --name file_kernel $BZ_IMAGE_FILENAME\n
 imgfetch --name file_initrd $DTS_IMAGE_FILENAME\n
 kernel file_kernel root=/dev/nfs initrd=file_initrd\n
 boot" > $IPXE_PATH/dts.ipxe
