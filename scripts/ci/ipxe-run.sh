c3b30173a01bb69ff467163a205c1cdf7ef29eed
Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
2024-12-03 14:59:59 +0100

    scripts/ci/ipxe-run.sh: Parametrize file paths
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>

--- scripts/ci/ipxe-run.sh
+++ scripts/ci/ipxe-run.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-mkdir ipxe
-cp build/tmp/deploy/images/genericx86-64/dts-base-image-genericx86-64.cpio.gz ipxe
-cp build/tmp/deploy/images/genericx86-64/bzImage ipxe
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <dts-base-image-path> <bzImage-path>"
+    exit 1
+fi
+
+DTS_IMAGE_PATH=$1
+DTS_IMAGE_FILENAME=$( basename $DTS_IMAGE_PATH)
+BZ_IMAGE_PATH=$2
+IPXE_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/../../ipxe
+
+echo $IPXE_PATH
+mkdir $IPXE_PATH
+cp $DTS_IMAGE_PATH $IPXE_PATH
+cp $BZ_IMAGE_PATH $IPXE_PATH
 
 echo -e "\n
 #!ipxe\n
 imgfetch --name file_kernel bzImage\n
-imgfetch --name file_initrd dts-base-image-genericx86-64.cpio.gz\n
+imgfetch --name file_initrd $DTS_IMAGE_FILENAME\n
 kernel file_kernel root=/dev/nfs initrd=file_initrd\n
-boot" > ipxe/dts.ipxe
-cd ipxe && python3 -m http.server 4321 &
+boot" > $IPXE_PATH/dts.ipxe
+cd $IPXE_PATH && python3 -m http.server 4321 &
