90fa5f00a74f3aea1f5bbc08816ad31cc6774a94
Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
2024-12-05 10:18:39 +0100

    scripts/ci/ipxe-run.sh: double-quote variables to prevent globbing (shellchek)
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>

--- scripts/ci/ipxe-run.sh
+++ scripts/ci/ipxe-run.sh
@@ -6,19 +6,19 @@ if [[ $# -ne 2 ]]; then
 fi
 
 DTS_IMAGE_PATH=$1
-DTS_IMAGE_FILENAME=$( basename $DTS_IMAGE_PATH )
+DTS_IMAGE_FILENAME=$( basename "$DTS_IMAGE_PATH" )
 BZ_IMAGE_PATH=$2
-BZ_IMAGE_FILENAME=$( basename $BZ_IMAGE_PATH )
+BZ_IMAGE_FILENAME=$( basename "$BZ_IMAGE_PATH" )
 IPXE_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/../../ipxe
 
-mkdir -p $IPXE_PATH
-ln -sf $DTS_IMAGE_PATH $IPXE_PATH/$DTS_IMAGE_FILENAME
-ln -sf $BZ_IMAGE_PATH $IPXE_PATH/$BZ_IMAGE_FILENAME
+mkdir -p "$IPXE_PATH"
+ln -sf "$DTS_IMAGE_PATH" "$IPXE_PATH"/"$DTS_IMAGE_FILENAME"
+ln -sf "$BZ_IMAGE_PATH" "$IPXE_PATH"/"$BZ_IMAGE_FILENAME"
 
 echo -e "\n
 #!ipxe\n
 imgfetch --name file_kernel $BZ_IMAGE_FILENAME\n
 imgfetch --name file_initrd $DTS_IMAGE_FILENAME\n
 kernel file_kernel root=/dev/nfs initrd=file_initrd\n
-boot" > $IPXE_PATH/dts.ipxe
-cd $IPXE_PATH && python3 -m http.server 4321
+boot" > "$IPXE_PATH"/dts.ipxe
+cd "$IPXE_PATH" && python3 -m http.server 4321
