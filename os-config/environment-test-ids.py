6f96f0386094eac6164ecc861db2b717f7a9c963
Filip Gołaś <filip.golas@3mdeb.com>
2025-02-24 17:35:04 +0100

    os-config/environment-test-ids: Change to .py and add ENV_ID to name map
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

---
+++ os-config/environment-test-ids.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2025 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# 1xx - Firmware
+ENV_ID_EDK2 = "101"
+ENV_ID_SEABIOS = "102"
+ENV_ID_IPXE = "103"
+ENV_ID_HEADS = "104"
+
+# 2xx - Linux
+ENV_ID_UBUNTU = "201"
+ENV_ID_FEDORA = "202"
+ENV_ID_QUBES = "203"
+
+# 3xx - Windows
+ENV_ID_WINDOWS_11 = "301"
+
+ENV_ID_OS_BOOTMENU_NAMES = {
+    ENV_ID_UBUNTU: "ubuntu",
+    ENV_ID_FEDORA: "fedora",
+    ENV_ID_WINDOWS_11: "windows",
+}
