fc455d8e75c20be235dc0da4b0b43fca276ac29a
Filip Gołaś <filip.golas@3mdeb.com>
2025-06-12 20:26:21 +0200

    scripts/ci: separate QEMU normal and stress-tests
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

---
+++ scripts/ci/qemu-stress-tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2024 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Define an array of commands
+
+commands=(
+  "robot -L TRACE -v config:qemu-selftests -v rte_ip:127.0.0.1 -d ./logs/$(date +%Y.%m.%d_%H.%M.%S)/setup-and-boot-menus -v snipeit:no --include stress-test self-tests/setup-and-boot-menus.robot"
+  "robot -L TRACE -v config:qemu-selftests -v rte_ip:127.0.0.1 -d ./logs/$(date +%Y.%m.%d_%H.%M.%S)/dasharo-system-features-menus -v snipeit:no --include stress-test self-tests/dasharo-system-features-menus.robot"
+  "robot -L TRACE -v config:qemu-selftests -v rte_ip:127.0.0.1 -d ./logs/$(date +%Y.%m.%d_%H.%M.%S)/boolean-options -v snipeit:no --include stress-test self-tests/boolean-options.robot"
+  "robot -L TRACE -v config:qemu-selftests -v rte_ip:127.0.0.1 -d ./logs/$(date +%Y.%m.%d_%H.%M.%S)/numerical-options -v snipeit:no --include stress-test self-tests/numerical-options.robot"
+  "robot -L TRACE -v config:qemu-selftests -v rte_ip:127.0.0.1 -d ./logs/$(date +%Y.%m.%d_%H.%M.%S)/list-options -v snipeit:no --include stress-test self-tests/list-options.robot"
+  "robot -L TRACE -v config:qemu-selftests -v rte_ip:127.0.0.1 -d ./logs/$(date +%Y.%m.%d_%H.%M.%S)/self-tests -v snipeit:no --include stress-test self-tests/terminal.robot"
+)
+
+# Initialize a variable to track overall success
+overall_success=0
+
+# Execute each command and capture the exit codes
+for cmd in "${commands[@]}"; do
+  eval $cmd
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    overall_success=1
+  fi
+done
+
+# Exit with the appropriate status
+exit $overall_success
