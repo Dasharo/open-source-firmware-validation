e20c07efbba3006b191e25aec88a24807952706f
Filip Gołaś <filip.golas@3mdeb.com>
2025-06-12 20:14:11 +0200

    develop_pr_auto_regression.sh: Add script for CI hardware regression
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

---
+++ scripts/ci/develop_pr_auto_regression.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2025 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mapfile -t commands < <(${SCRIPT_DIR}/regression-scope/osfv_regression_scope.py commands --compare_to develop)
+
+pids=()
+statuses=()
+LOGS_DIR="./logs"
+mkdir -f $LOGS_DIR/
+i=1
+for command in "${commands[@]}"; do
+    (
+        echo "Executing: ${command}"
+        eval ${command} > "$LOGS_DIR/run_${i}.log" 2>&1
+    ) &
+    pids[${i}]=$!
+    (( i++ ))
+done
+
+sleep 1
+
+for idx in "${!pids[@]}"; do
+    echo "Waiting for run $idx to finish..."
+    if wait ${pids[$idx]}; then
+        statuses[idx]=0
+        echo "run $idx is done."
+    else
+        statuses[idx]=$?
+        echo "run $idx failed."
+    fi
+done
+
+
+exit_code=0
+i=0
+for code in "${statuses[@]}"; do
+    if [[ "$code" -ne 0 ]]; then
+        echo "Run ${i} failed with exit code ${code}. Check $LOGS_DIR/run_${i}.log for details."
+        exit_code=1
+    fi
+    (( i++))
+done
+
+exit $exit_code
