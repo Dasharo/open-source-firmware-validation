baea00d7d99eec2be70df6adda39fcb064fdf006
Filip Gołaś <filip.golas@3mdeb.com>
2025-06-17 09:37:48 +0200

    scripts/ci/test_ids: Add CI scripts for testing test IDs

---
+++ scripts/ci/test_ids/test_id_checking_lib.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2025 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from robot.model import SuiteVisitor
+from robot.running import TestSuiteBuilder
+import re
+import subprocess
+import difflib
+
+# Maybe use os-config/environment-test-ids.py instead???
+# A mapping from id to some name is missing though
+# and the bootmanager entries might not be perfect to
+# insert into the braces in test names
+OS_IDS = {
+    r"\.201": r"\(Ubuntu\)",
+    r"\.301": r"\(Windows\)",
+    r"\.202": r"\(Fedora\)",
+}
+
+
+class TestCasesFinder(SuiteVisitor):
+    def __init__(self):
+        self.tests = []
+
+    def visit_test(self, test):
+        self.tests.append(test)
+
+def get_test_cases_from_dir(directory):
+    builder = TestSuiteBuilder()
+    try:
+        testsuite = builder.build(directory)
+    except Exception as e:
+        print(
+            f"Error building test suite from {directory}: {e}. Assuming no test cases."
+        )
+        return []
+    finder = TestCasesFinder()
+    testsuite.visit(finder)
+
+    list_of_tests = finder.tests
+
+    return list_of_tests
+
+
+def get_id(test):
+    res = re.search('^([A-Z]{0,9}[0-9]{3}\\.[0-9]{3}).*', test.name)
+    if res:
+        return res.group(1)
+    return None
+
+def id_valid(test):
+    return get_id(test) is not None
+
+def os_valid(test):
+    for os_id in OS_IDS.keys():
+        if re.search(os_id, test.name) and not re.search(OS_IDS[os_id], test.name):
+            return False
+    return True
+
+def check_mappings():
+    return(len(compare_mappings())==0)
+
+def compare_mappings():
+    robot = subprocess.run('./scripts/list-tests-from-robot.sh', capture_output=True, text=True)
+    json = subprocess.run('./scripts/list-tests-from-json.sh', capture_output=True, text=True)
+    diff = difflib.unified_diff(
+        str.splitlines(robot.stdout),
+        str.splitlines(json.stdout),
+        fromfile='robot',
+        tofile='json',
+        lineterm=''
+    )
+    return list(diff)
---
+++ scripts/ci/test_ids/test_ids_legal.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2025 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+import test_id_checking_lib as lib
+import sys
+import fire
+
+
+paths = [
+    "dasharo-compatibility",
+    "dasharo-performance",
+    "dasharo-security",
+    "dasharo-stability",
+]
+
+def check_tests(check_function):
+    invalid_tests = []
+    for path in paths:
+        tests = lib.get_test_cases_from_dir(path)
+        for t in tests:
+            if not check_function(t):
+                invalid_tests.append(t)
+    return invalid_tests
+
+class CLI():
+    def id_valid(self, print_invalid=False):
+        """Checks if the IDs of tests are not illegal
+
+        print_invalid: Print invalid test names
+        """
+        invalid_ids = check_tests(lib.id_valid)
+        if print_invalid:
+            for test in invalid_ids:
+                print(test.name)
+
+        if len(invalid_ids) == 0:
+            sys.exit(0)
+        else:
+            sys.exit(1)
+
+    def os_valid(self, print_invalid=False):
+        """Checks if the OS Ids are valid and represented in test name
+
+        print_invalid: Print invalid test names
+        """
+        invalid_ids = check_tests(lib.os_valid)
+        if print_invalid:
+            for test in invalid_ids:
+                print(test.name)
+
+        if len(invalid_ids) == 0:
+            sys.exit(0)
+        else:
+            sys.exit(1)
+
+    def id_mapped(self, print_invalid=False):
+        """Checks if the test_cases.json test ID mappings are correct
+
+        print_invalid: Print the invalid mappings
+        """
+        if print_invalid:
+            for diff in lib.compare_mappings():
+                print(diff)
+
+        if(lib.check_mappings()):
+            sys.exit(0)
+        else:
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    fire.Fire(CLI())
