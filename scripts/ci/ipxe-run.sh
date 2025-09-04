55faf17071127dbc4bc8296d85ababa14dad62c7
Michał Iwanicki <michal.iwanicki@3mdeb.com>
2025-09-04 11:21:53 +0200

    scripts/ci/ipxe-run.sh: add console=ttyUSB0 for ftdi serial (#1051)
    Used on Novacustom platforms for serial console
    
    Signed-off-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

--- scripts/ci/ipxe-run.sh
+++ scripts/ci/ipxe-run.sh
@@ -23,7 +23,7 @@ cat <<EOF > "$IPXE_PATH/dts.ipxe"
 #!ipxe
 imgfetch --name file_kernel $BZ_IMAGE_FILENAME
 imgfetch --name file_initrd $DTS_IMAGE_FILENAME
-kernel file_kernel initrd=file_initrd
+kernel file_kernel initrd=file_initrd console=ttyUSB0
 boot
 EOF
 
