70039ff75a1d90b780f9501662a8dd33d984d05f
Michał Kopeć <michal.kopec@3mdeb.com>
2026-01-09 11:49:39 +0100

    scripts/capsules: upload BTG capsule if needed
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- scripts/capsules/prepare_capsule_update_tests_drive.sh
+++ scripts/capsules/prepare_capsule_update_tests_drive.sh
@@ -61,10 +61,12 @@ cp $FILES_DIR/variable_capsule_file.nsh "$mount_point"/variable_capsule_file.nsh
 cp $FILES_DIR/variable_step.nsh "$mount_point"/variable_step.nsh
 cp $CAPSULE_FILES_DIR/${CAPSULE_BASENAME}_wrong_cert.cap "$mount_point"/wrong_cert.cap
 cp $CAPSULE_FILES_DIR/${CAPSULE_BASENAME}_invalid_guid.cap "$mount_point"/invalid_guid.cap
-cp $CAPSULE_FILES_DIR/${CAPSULE_BASENAME}_invalid_btg_signature.cap "$mount_point"/invalid_btg_signature.cap
 cp $CAPSULE_FW_FILE "$mount_point"/valid_capsule.cap
 cp $FW_FILE "$mount_point"/pre-capsule.rom
 
+if [ "$BTG_CAPSULE_FW_FILE" != "" ]; then
+    cp "$BTG_CAPSULE_FW_FILE" "$mount_point"/invalid_btg_signature.cap
+fi
 
 # cleanup
 udisksctl unmount -b $DRIVE
