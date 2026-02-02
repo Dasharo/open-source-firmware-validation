b8f413b9ddf3663196b1539ff8c4b0e07069643a
Filip Gołaś <filip.golas@3mdeb.com>
2026-02-02 16:04:46 +0100

    osfv-regression-scope.py: Allow predefined tests list instead of smart scope
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- scripts/ci/regression-scope/osfv_regression_scope.py
+++ scripts/ci/regression-scope/osfv_regression_scope.py
@@ -11,6 +11,7 @@ import sys
 from pprint import pprint
 
 import fire
+import pandas as pd
 
 from lib.parser_manager import ParserManager
 from lib.rules_parser import RuleParser
@@ -37,44 +38,57 @@ def get_changed_files(compare_to):
     return files
 
 
+def get_files_from_list(list_path):
+    """
+    Returns a list of filenames from a list of files.
+    List is a TSV in form of:
+    <test ID>\t<Name>\t<Automation>\t<Result>\t<Has comment>\n
+    Fields other than <test ID> are optional for easy copying from dashboard.
+    """
+    table = pd.read_csv(list_path, header=None)
+    return table.iloc[:, 0].tolist()
+
+
 class CLI:
-    def filenames(
-        self, rules_file="scripts/ci/regression-scope/rules.json", compare_to="HEAD"
-    ):
+    def __init__(self, override_tests_list=None, compare_to="HEAD"):
+        self.compare_to = compare_to
+        self.override_tests_list = override_tests_list
+        if override_tests_list is None:
+            self.get_changed_files = lambda: get_changed_files(compare_to)
+        else:
+            self.get_changed_files = lambda: get_files_from_list(override_tests_list)
+
+    def filenames(self, rules_file="scripts/ci/regression-scope/rules.json"):
         """
         Print the filenames of test suites that are affected by the changes
         """
         with open(rules_file) as rules_file:
             self.rules = json.load(rules_file)["rules"]
-        self.changed_files = get_changed_files(compare_to)
+        self.changed_files = self.get_changed_files()
         parser = ParserManager(self.rules, self.changed_files)
         parser.parse()
         print(" ".join(parser.files()))
 
-    def commands(
-        self, rules_file="scripts/ci/regression-scope/rules.json", compare_to="HEAD"
-    ):
+    def commands(self, rules_file="scripts/ci/regression-scope/rules.json"):
         """
         Print the commands that should be executed to test the changes
         """
         with open(rules_file) as rules_file:
             self.rules = json.load(rules_file)["rules"]
-        self.changed_files = get_changed_files(compare_to)
+        self.changed_files = self.get_changed_files()
         parser = ParserManager(self.rules, self.changed_files)
         parser.parse()
         for command in parser.commands():
             print(" ".join(command))
 
-    def robot_args(
-        self, rules_file="scripts/ci/regression-scope/rules.json", compare_to="HEAD"
-    ):
+    def robot_args(self, rules_file="scripts/ci/regression-scope/rules.json"):
         """
         Print the parameters that should be passed to the run.sh robot wrapper to
         test the changes. Does not
         """
         with open(rules_file) as rules_file:
             self.rules = json.load(rules_file)["rules"]
-        self.changed_files = get_changed_files(compare_to)
+        self.changed_files = self.get_changed_files()
         parser = ParserManager(self.rules, self.changed_files)
         parser.parse()
         print(" ".join(parser.wrapper_args()))
