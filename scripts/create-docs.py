c476aa4c427ec7789f66f9abdf92e328714e8e2c
ditirodt <ditirodt@users.noreply.github.com>
2026-06-29 17:45:02 +0200

    Include Python keyword libraries in docs generation

--- scripts/create-docs.py
+++ scripts/create-docs.py
@@ -7,16 +7,20 @@
 import os
 import re
 import sys
+import tempfile
 
 lib_dir = "lib"
 output_resource = sys.argv[1] if len(sys.argv) > 1 else "all-keywords.robot"
 
-# Collect all .robot files
+# Collect all keyword libraries
 robot_files = ["keywords.robot"]
+python_files = []
 for root, dirs, files in os.walk(lib_dir):
     for file in files:
         if file.endswith(".robot"):
             robot_files.append(os.path.join(root, file))
+        elif file.endswith(".py"):
+            python_files.append(os.path.join(root, file))
 
 # Regex pattern to detect keyword definitions
 keyword_pattern = re.compile(
@@ -24,9 +28,17 @@ keyword_pattern = re.compile(
 )
 
 delimiter = "."  # Between filename and the keyword
+qemu_monitor_socket = os.path.join(tempfile.gettempdir(), "osfv-libdoc-qemu-monitor")
+open(qemu_monitor_socket, "a", encoding="utf8").close()
 
 # Create the combined resource file with prefixed keywords
 with open(output_resource, "w", encoding="utf8") as res_file:
+    for python_file in python_files:
+        if os.path.basename(python_file) == "QemuMonitor.py":
+            res_file.write(f"Library    {python_file}    {qemu_monitor_socket}\n")
+        else:
+            res_file.write(f"Library    {python_file}\n")
+    res_file.write("\n")
 
     for robot_file in robot_files:
         file_prefix = os.path.splitext(os.path.basename(robot_file))[0] + delimiter
